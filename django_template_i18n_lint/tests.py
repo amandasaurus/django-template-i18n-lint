aeef6d183e7a1e922eeee259d97f2afb8876d481
Rory McCann <rory@technomancy.org>
2013-11-20 21:16:02 +0000

    more tests

--- django_template_i18n_lint/tests.py
+++ django_template_i18n_lint/tests.py
@@ -13,6 +13,15 @@ class DjangoTemplateI18nLintTestCase(unittest.TestCase):
     testMultiWord = _known_good_output("<h1>Foo</h1><p>Bar</p>", [(1, 5, 'Foo'), (1, 16, 'Bar')])
     testMultiWordMultiLine = _known_good_output("<h1>Foo</h1>\n<p>Bar</p>", [(1, 5, 'Foo'), (2, 4, 'Bar')])
 
+    # Test things that shouldn't be included
+    testTransOK = _known_good_output("<b>{% trans 'Foo' %}</b>", [])
+    testBlocktransOK1 = _known_good_output("<b>{% blocktrans %}Foo{% endblocktrans %}</b>", [])
+    testBlocktransOK2 = _known_good_output("<b>{% blocktrans with var=bar %}Foo{% endblocktrans %}</b>", [])
+    testBlocktransOK3 = _known_good_output("<b>{% blocktrans with var as bar %}Foo{% endblocktrans %}</b>", [])
+    testDjangoCustomTag = _known_good_output("{% load foo %}", [])
+    testJS = _known_good_output("Foo<script>alert('Foo');</script>Bar", [(1, 1, 'Foo'), (1, 34, 'Bar')])
+    testDjangoVar = _known_good_output("Foo{{ bar }}Baz", [(1, 1, 'Foo'), (1, 13, 'Baz')])
+
 
 if __name__ == '__main__':
     unittest.main()
