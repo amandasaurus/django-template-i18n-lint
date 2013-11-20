613f0911b481c8cb89235958f3ba71d2be07e23d
Rory McCann <rory@technomancy.org>
2013-11-20 21:35:55 +0000

    some code formatting

--- django_template_i18n_lint/tests.py
+++ django_template_i18n_lint/tests.py
@@ -1,13 +1,23 @@
+"""
+Unittests for django-template-i18n-lint.
+"""
 import unittest
 import django_template_i18n_lint
 
+
+def _known_good_output(input_string, expected_output):
+    def test(self):
+        actual_output = list(django_template_i18n_lint.non_translated_text(input_string))
+        self.assertEqual(actual_output, expected_output)
+    test.__doc__ = "Input string {0} should give output of {1}".format(
+        repr(input_string[:30]), repr(expected_output)[:30])
+    return test
+
+
 class DjangoTemplateI18nLintTestCase(unittest.TestCase):
-    def _known_good_output(input_string, expected_output):
-        def test(self):
-            actual_output = list(django_template_i18n_lint.non_translated_text(input_string))
-            self.assertEqual(actual_output, expected_output)
-        test.__doc__ = "Input string {0} should give output of {1}".format(repr(input_string[:30]), repr(expected_output)[:30])
-        return test
+    """
+    Unittests.
+    """
 
     testSimple = _known_good_output("<h1>Foo</h1>", [(1, 5, 'Foo')])
     testMultiWord = _known_good_output("<h1>Foo</h1><p>Bar</p>", [(1, 5, 'Foo'), (1, 16, 'Bar')])
